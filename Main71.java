5b29d055ed8b64a46d543f80805b1bb077eac722
avgustvik <avgust.vik@gmail.com>
2020-04-06 17:54:33 +0300

    This is Automation started

---
+++ Main71.java
@@ -0,0 +1,28 @@
+package com.gmail.vik.avgust;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main71 {
+
+	public static void main(String[] args) {
+
+		Calendar cl1 = Calendar.getInstance();
+		
+		Calendar cl2 = Calendar.getInstance();
+		cl2.add(Calendar.MONTH, -1);
+
+		Date date1 = cl1.getTime();
+		Date date2 = cl2.getTime();
+
+		System.out.println(date1);
+		System.out.println(date2);
+
+		long t1 = date1.getTime();
+		long t2 = date2.getTime();
+
+		long result = t1 - t2;
+
+		System.out.println("Difference is " + result);
+	}
+}
